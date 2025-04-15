fa067a70904a96977461a01f1a1ce8ef808bdcf5
Allen Chen <12173+ac2324@users.noreply.github.coecis.cornell.edu>
2025-04-15 00:51:18 -0400

    Update app.py
    - efficient import
    - functionality to use new SVD and data

--- backend/app.py
+++ backend/app.py
@@ -2,17 +2,15 @@ import json
 import os
 from flask import Flask, render_template, request
 from flask_cors import CORS
-from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
 import pandas as pd
-from scipy.sparse.linalg import svds
 
-# from nltk.tokenize import TreebankWordTokenizer
-from sklearn.feature_extraction.text import TfidfVectorizer
-import math
 import numpy as np
 from sklearn.preprocessing import normalize
 
+import gc
 import joblib
+
+NUM_INDICES_IN_EACH_DATA_FILE = 3828  # determined by running create_init_json
 
 # ROOT_PATH for linking with all your files.
 # Feel free to use a config.py or settings.py with a global export variable
@@ -21,15 +19,18 @@ os.environ["ROOT_PATH"] = os.path.abspath(os.path.join("..", os.curdir))
 # Get the directory of the current script
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
-# Specify the path to the JSON file relative to the current script
-json_file_path = os.path.join(current_directory, "init.json")
 
-# Assuming your JSON data is stored in a file named 'init.json'
-with open(json_file_path, "r") as file:
-    data = json.load(file)
+def get_path(file_name):
+    return os.path.join(current_directory, file_name)
 
-vectorizer = joblib.load("tfidf_vectorizer.pkl")
-words_compressed = np.load("words_compressed.npy")
+
+# IF WE NEED MORE MEMORY, LOAD ON DEMAND AND IMMEDIATELY DELETE AFTERWARD
+vectorizer = joblib.load(get_path("tfidf_vectorizer.pkl"))
+words_compressed = np.load(get_path("words_compressed_0.npy"))
+num_split = 5
+for k in range(1, num_split):
+    new_words_compressed = np.load(get_path("words_compressed_" + str(k) + ".npy"))
+    words_compressed = np.concatenate((words_compressed, new_words_compressed))
 docs_compressed_normed = normalize(np.load("docs_compressed.npy"))
 
 app = Flask(__name__)
@@ -47,11 +48,21 @@ def json_search(query):
     asort = np.argsort(sims)[::-1][:10]
     matched_res = []
     for i in range(10):
-        matched_res.append(data[asort[i]])
+        index = asort[i]
+        file_num = int(index / NUM_INDICES_IN_EACH_DATA_FILE)
+        # here, we should load the relevant data
+        with open(get_path("data_" + str(file_num) + ".json"), "r") as file:
+            data = json.load(file)
+        index_in_file = index - NUM_INDICES_IN_EACH_DATA_FILE * file_num
+        # then grab the data and append
+        matched_res.append(data[index_in_file])
+        # then delete the resource
+        del data
+        # then have garbage collector collect it
+        gc.collect()
 
     df = pd.DataFrame(matched_res)
     df["similarity_score"] = scores
-    # TODO: Add photos, hyperlink, ISSN
     return df.to_json(orient="records")
 
 
