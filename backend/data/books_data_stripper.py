ab56005ee0752f3fed219139dc6e5ce3755c576a
Allen Chen <12173+ac2324@users.noreply.github.coecis.cornell.edu>
2025-04-16 09:54:44 -0400

    removed unnecessary code

--- backend/data/books_data_stripper.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("books_data.csv")
-df = df.drop(columns=["image", "previewLink", "publisher", "publishedDate"])
-
-df.to_csv("books_data_stripped.csv", index=False)
