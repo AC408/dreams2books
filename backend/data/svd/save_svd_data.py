5728aa9bd68f438a665ccf186fa46be27d80259f
Allen Chen <12173+ac2324@users.noreply.github.coecis.cornell.edu>
2025-04-22 14:09:53 -0400

    attempt at conserving memory

--- backend/data/svd/save_svd_data.py
+++ backend/data/svd/save_svd_data.py
@@ -33,7 +33,7 @@ for d in data:
 
 tf_idf = vectorizer.fit_transform([d for d in doc_terms])
 
-docs_compressed, s, words_compressed = svds(tf_idf, k=125)
+docs_compressed, s, words_compressed = svds(tf_idf, k=50)
 
 num_splits = 5
 split = np.split(words_compressed, num_splits)
