be0bd184dedc65e9c5df694b05c7b1dfe1e3d25b
Allen Chen <12173+ac2324@users.noreply.github.coecis.cornell.edu>
2025-04-22 12:03:11 -0400

    updated constants to get max data

--- backend/create_init_json.py
+++ backend/create_init_json.py
@@ -2,16 +2,16 @@ import pandas as pd
 import json
 import re
 
-MIN_NUM_REVIEW = 4  # Each books need MIN_NUM_REVIEW reviews
+MIN_NUM_REVIEW = 2  # Each books need MIN_NUM_REVIEW reviews
 MIN_DESCRIPTION_CHAR = 20  # Each book descriptions need MIN_DESCRIPTION_CHAR chars
 MIN_REVIEW_HELPFULNESS = (
-    0.8  # Each review has to be at least MIN_REVIEW_HELPFULNESS helpful
+    0.6  # Each review has to be at least MIN_REVIEW_HELPFULNESS helpful
 )
 MIN_NUM_REVIEW_OF_REVIEW = (
-    8  # Each review needs to have MIN_NUM_REVIEW_OF_REVIEW reviews
+    2  # Each review needs to have MIN_NUM_REVIEW_OF_REVIEW reviews
 )
 MIN_REVIEW_CHAR = 20  # Each review needs MIN_REVIEW_CHAR chars
-MAX_REVIEW_COUNT_PER_BOOK = 3  # Max number of reviews per book to conserve space
+MAX_REVIEW_COUNT_PER_BOOK = 10  # Max number of reviews per book to conserve space
 
 path_to_books_data = "~/Downloads/archive/books_data.csv"
 
@@ -204,7 +204,7 @@ df_books_data = pd.DataFrame(parsed_books)
 # turn to dictionary to store as json
 df_dic = df_books_data.to_dict(orient="records")
 num_dictionary = len(df_dic)
-num_splits = 1
+num_splits = 10
 num_steps = int(num_dictionary / num_splits) + 1
 print(num_steps)
 start = 0
