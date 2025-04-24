6ee582d9686a02ae6799ce8d1f77154eb1c15b14
Allen Chen <12173+ac2324@users.noreply.github.coecis.cornell.edu>
2025-04-24 16:30:21 -0400

    update dimension
    - fixed comments
    - updated with new svd

--- backend/data/categories/latent_space.py
+++ backend/data/categories/latent_space.py
@@ -4,37 +4,35 @@ from sklearn.preprocessing import normalize
 import json
 import gc
 
-NUM_INDICES_IN_EACH_DATA_FILE = 11450  # determined by running create_init_json
+NUM_INDICES_IN_EACH_DATA_FILE = 11309  # determined by running create_init_json
 
 root_dir = os.path.abspath(os.path.join("..", os.curdir))
-svd_path = root_dir + "/svd/"
 
-docs_compressed_normed = normalize(np.load(svd_path + "docs_compressed.npy"))
+docs_compressed_normed = normalize(np.load(root_dir + "/data/svd/docs_compressed.npy"))
 
-U_30 = docs_compressed_normed[
-    :, -30:
-]  # SVD gives eigenvalues (dimensions) in ascending order, we want the most variabilities
-print(U_30.shape)
+# SVD gives eigenvalues (dimensions) in ascending order, we want the most variabilities
+U_30 = docs_compressed_normed[:, -30:]
+print(U_30.shape)  # double checking shape
+
 for i in range(30):
-    top_docs = U_30[:, i].argsort()[::-1][
-        :30
-    ]  # Get top 30 documents most related to each dimensions
-    print(f"Top documents for dimension {i}: {top_docs[:5]}")
+    # Get top 30 documents most related to each dimension
+    top_docs = U_30[:, i].argsort()[::-1][:30]
+
     matched_res = []
     for j in range(30):
         index = top_docs[j]
         file_num = int(index / NUM_INDICES_IN_EACH_DATA_FILE)
         # here, we should load the relevant data
-        with open(root_dir + "/book_info/data_" + str(file_num) + ".json", "r") as file:
+        with open(
+            root_dir + "/data/book_info/data_" + str(file_num) + ".json", "r"
+        ) as file:
             data = json.load(file)
         index_in_file = index - NUM_INDICES_IN_EACH_DATA_FILE * file_num
         # then grab the data and append
         matched_res.append(data[index_in_file])
-        # then delete the resource
-        del data
-        # then have garbage collector collect it
-        gc.collect()
 
     # Write to a JSON file
-    with open("latent_dimensions" + str(i) + ".json", "w") as f:
+    with open(
+        root_dir + "/data/categories/latent_dimensions" + str(i) + ".json", "w"
+    ) as f:
         json.dump(matched_res, f, indent=4)  # indent makes it pretty
